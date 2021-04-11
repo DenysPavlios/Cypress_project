f6e869b27eb6bb13f66a0d643ab22dce1b8ec2eb
DenisPavlichenko <denis.pavlichenko.it@gmail.com>
2021-04-11 20:44:23 +0300

    cy.contains

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,1 +1,25 @@
 ///  <reference types="Cypress" /> 
+
+ 
+// it('', () => {
+//       cy.visit('https://next.privat24.ua/mobile?lang=en')
+//       cy.contains('Sign in')
+// })
+
+// it('', () => {
+//       cy.visit('https://next.privat24.ua/mobile?lang=en')
+//       cy.contains('div', 'Sign in')
+// })
+
+// it('', () => {
+//       cy.visit('https://next.privat24.ua/mobile?lang=en')
+//       cy.contains('SIGN IN', {matchCase: false})
+// })
+
+// it.only('', () => {
+//       cy.viewport(1800, 700)
+//       cy.visit('https://next.privat24.ua/mobile?lang=en')
+//       cy.get('footer').contains('Go to old version')  // кнопка имеет более приоритет чем ссылка
+// })
+
+
