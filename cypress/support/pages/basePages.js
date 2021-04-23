2940975aeb392824446d9fb06e740ebf462a6b68
DenisPavlichenko <64361607+DenisPavlichenko@users.noreply.github.com>
2021-04-23 17:01:03 +0300

    Update basePages.js

--- cypress/support/pages/basePages.js
+++ cypress/support/pages/basePages.js
@@ -1,32 +1,28 @@
-export class BasePages {
+export class BasePage{
 
-open(url) {
-    cy.visit(url)
-    }       
-    
-typeAmount(typeamount) {
-    cy.get('[data-qa-node="amount"]')
-        .type(typeamount)
-    }
-    
-typeDebitCardData(cardNumber, expDate, cvv){
-    cy.get('[data-qa-node="numberdebitSource"]')
-        .type(cardNumber)
-        .get('[data-qa-node="expiredebitSource"]')
-        .type(expDate)
-        .get('[data-qa-node="cvvdebitSource"]')
-        .type(cvv)
-    }
-submitPayment () {
-    cy.get('[type="submit"]')
-        .click()
+    open(url){
+        cy.visit(url);
     }
 
+    typeAmount(amount){
+        cy.get('[data-qa-node="amount"]')
+             .type(amount)
+     }
 
+     typeDebitCardData(cardNumber, expDate, cvv){
+        cy.get('[data-qa-node="numberdebitSource"]')
+            .type(cardNumber)
+            .get('[data-qa-node="expiredebitSource"]')
+            .type(expDate)
+            .get('[data-qa-node="cvvdebitSource"]')
+            .type(cvv)
+    }
 
+    submitPayment(){
+        cy.get('button[type="submit"]')
+             .click({force: true})
+     }
 
 }
 
-
-
-export const basePages = new BasePages()
+export const basePage = new BasePage()
