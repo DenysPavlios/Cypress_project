600e7b55b6bbaa90cdaba3330c01ec302c83a15a
DenisPavlichenko <64361607+DenisPavlichenko@users.noreply.github.com>
2021-04-23 17:01:37 +0300

    Update mobileReplenishment.js

--- cypress/support/pages/mobileReplenishment.js
+++ cypress/support/pages/mobileReplenishment.js
@@ -1,42 +1,38 @@
-export class mobilePhoneReplenishment {
-typePhoneNumper(phoneNumber){
-    cy.get('[data-qa-node="phone-number"]')
-        .type(phoneNumber)
-    }
-
-typeAmount(typeamount) {
-    cy.get('[data-qa-node="amount"]')
-        .type(typeamount)
-    }
-
-checkDebitCard(debitcard) {
-    cy.get('[data-qa-node="card"]')
-      .should('have.text', debitcard)
-    }
-
-chceckDebitAmount(debitAmount) {
-    cy.get('[data-qa-node="amount"]')
-      .should('have.text', debitAmount)
-    }
-
-chceckDebitAmountAndComission (debitComission) {
-    cy.get('[data-qa-node="commission"]')
-      .eq(1)
-      .should('contain.text', debitComission)
-    }
-
-checkReceiverAmount(receiverAmount) {
-    cy.get('.amount')
-        .find("span")
-        .should("have.text", receiverAmount)
-    }
-
-checkPaymentCurrency(paymentCurrency) {
-    cy.get(".amount")
-        .find("small")
-        .should('contain.text', "UAH")
-    }
+export class MobilePhoneReplenishment {
     
+    typePhoneNumber(phoneNumber){
+        cy.get('[data-qa-node="phone-number"]')
+            .type(phoneNumber)
+    }
+
+    checkDebitCard(debitCard){
+        cy.get('[data-qa-node="card"]')
+            .should("have.text", debitCard)
+    }
+
+    checkDebitAmount(debitAmount){
+        cy.get('[data-qa-node="amount"]')
+            .should("have.text", debitAmount)
+    }
+
+    checkDebitAmountAndComission(debotComission){
+        cy.get('[data-qa-node="commission"]')
+            .eq(1)
+            .should("have.text", debotComission)
+    }
+
+    checkReceiverAmount(receiverAmount){
+        cy.get(".amount")
+            .find("span")
+            .should("have.text", receiverAmount)
+    }
+
+    checkPaymentCurrency(paymentCurrency){
+        cy.get(".amount")
+            .find("small")
+            .should("contain.text", paymentCurrency);
+    }
 }
-export const mobileReplenishment = new mobilePhoneReplenishment()
+
+export const mobileReplenishment = new MobilePhoneReplenishment()
 
