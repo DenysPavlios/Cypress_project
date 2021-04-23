d86fe4f821ed9656254ed49c82328b6a232938f4
DenisPavlichenko <64361607+DenisPavlichenko@users.noreply.github.com>
2021-04-23 16:45:40 +0300

    Update firstTest.spec.js

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,132 +1,96 @@
-///  <reference types="Cypress" /> 
-it('type', () => {
-      cy.viewport(1800, 700)
-      cy.visit('https://test.buildgcp.com/login')
-      .get('[name="login"]')
-      .type('test-user')
+/// <reference types="Cypress" />
+
+it('type', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="phone-number"]')
+        .type(112233344)
 })
 
-it('check is correct URL', () => {
-      cy.viewport(1800, 700)
-      cy.visit('https://test.buildgcp.com/login')
-      cy.url()
-      .should('eq', 'https://test.buildgcp.com/login')
-      
-})  
+it('focus', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="amount"]')
+        .focus()
+})
 
+it('blur', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="amount"]')
+        .focus()
+        .blur()
+})
 
-it('focus', () => {
-      cy.viewport(1800, 700)
-      .visit('https://test.buildgcp.com/login')
-      .get('[name="login"]')
-      .focus()
-      
-})  
+it('clear', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="amount"]')
+        .type(999)
+        .wait(2000)
+        .clear()
+})
 
-it('blur', () => {
-      cy.viewport(1800, 700)
-      .visit('https://test.buildgcp.com/login')
-      .get('[name="login"]')
-      .focus()
-      .blur()
-      
-})  
+it('submit', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('form[method="post"]')
+        .submit()
+})
 
-it('clear', () => {
-      cy.viewport(1800, 700)
-      .visit('https://test.buildgcp.com/login')
-      .get('[name="login"]')
-      .type('User-Test')
-      .wait(5000)
-      .clear()
+it('click', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-value="manual"]')
+        .click()
+})
 
-})   
-it('submit', () => {
-      cy.viewport(1800, 700)
-      .visit('https://test.buildgcp.com/login')
-      .get('form[class="jss6"]')
-      .submit()
+it('rightclick', ()=>{
+    cy.visit('https://example.cypress.io/commands/actions')
+        .contains('Right click to edit')
+        .rightclick()
+})
 
-})   
-  
+it('dblclick', ()=>{
+    cy.visit('https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/API/Element/dblclick_event/_samples_/Examples')
+        .contains('My Card')
+        .dblclick()
+})
 
-it('click', () => {
-      cy.viewport(1800, 700)
-      .visit('https://test.buildgcp.com/login')
-      .get('span[class="MuiButton-label jss21"]')
-      .click()
+it('check', ()=>{
+    cy.visit('https://www.facebook.com/reg/?')
+        .get('input[value="2"]')
+        .check()
+})
 
-})   
+it('uncheck', ()=>{
+    cy.visit('https://en.privatbank.ua/')
+        .get('#switch-input')
+        .check({force: true})
+        .wait(2000)
+        .uncheck({force: true})
+})
 
-it('rightclick', () => {
-      cy.viewport(1800, 700)
-      .visit('https://example.cypress.io/commands/actions')
-      .contains('Right click to edit')
-      .rightclick()
+it('select', ()=>{
+    cy.visit('https://www.facebook.com/r.php?locale=en_US')
+        .get('#month')
+        .select('Feb')
+        .get('#day')
+        .select('12')
+        .get('#year')
+        .select('1991')
+})
 
-})   
+it('scrollIntoView', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="lang"]')
+        .wait(2000)
+        .scrollIntoView()
+})
 
-it('dbclick', () => {
-      cy.viewport(1800, 700)
-      .visit('https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/API/Element/dblclick_event/_samples_/Examples')
-      .contains('My Card')
-      .dblclick()
+it('scrollTo', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    .wait(2000)
+    cy.scrollTo(0, 500)
+})
 
-})   
-  
-
-it('check', () => {
-      cy.viewport(1800, 700)
-      .visit('https://www.facebook.com/reg/')
-      .get('input[value="2"]')
-      .check()
-
-})  
-
-it('uncheck', () => {
-      cy.viewport(1800, 800)
-      .visit('https://en.privatbank.ua/')
-      .get('#switch-input')
-      .check({force: true})
-      .wait(4000)
-      .uncheck({force: true})
-
-})  
-
-it('select', () => {
-      cy.viewport(1800, 800)
-      .visit('https://www.facebook.com/r.php?locale=en_US&display=page')
-      .get('#month')
-      .select('Jul')
-      .get('#day')
-      .select('29')
-      .get('#year')
-      .select('1985')
-      
-})  
-
-it('scroll', () => {
-      cy.viewport(1800, 700)
-      .visit('https://next.privat24.ua/')
-      .get('[data-qa-node="lang"]')
-      .wait(2000)
-      .scrollIntoView()
-
-})  
-it('cy.scroll', () => {
-      cy.viewport(1800, 700)
-      .visit('https://next.privat24.ua/')
-      .wait(2000)
-      cy.scrollTo(0, 500)
-
-})  
-
-it.only('check', () => {
-      cy.viewport(1800, 700)
-      .visit('https://next.privat24.ua/?lang=en')
-      .wait(3000)
-      .contains('Services')
-      .wait(3000)
-      .trigger('mouseover')
-
-})  
+it.only('check', ()=>{
+    cy.visit('https://next.privat24.ua/?lang=en')
+        .contains('Services')
+        .wait(2000)
+        .trigger('mouseover')
+})
